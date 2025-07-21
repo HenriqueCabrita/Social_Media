ef4795502b02650ee12702448d4cd7fae073ad50
kulhas <hmrc_aim@hotmail.com>
2025-07-21 12:32:00 +0100

    deployment

--- socialproject/socialproject/settings.py
+++ socialproject/socialproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*h$c1mtr8=7$-kktsqs%fznbfo(!r#coh&059$^nd*m1*t784_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['socialmedia.henrique-cabrita.com', 'localhost']
 
 
 # Application definition
